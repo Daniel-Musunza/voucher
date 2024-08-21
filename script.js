783b9072562102ae3db4c0c105df538d30e1dbba
Daniel-Musunza <musunzafestus2019@gmail.com>
2024-08-21 14:12:10 +0300

    in

---
+++ script.js
@@ -0,0 +1,105 @@
+const voucherData = {
+    holiday: {
+        destination: "MARRAKECH",
+        country: "MOROCCO",
+        coverImage: "images/firstimage.jpg",
+        holidayReferece: "6062974",
+        flightReference: "K7W5V28",
+        hotelDetails: {
+            hotelName: "Diwane Marrakech",
+            city: "Marrakech",
+            stars: 4,
+            reviews: 1551,
+            images: [
+                "https://www.i-escape.com/gallery/252001_253000/252601_252700/252677_840x540.jpg",
+                "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/df/59/4b/dar-zemora.jpg?w=700&h=-1&s=1",
+                "https://pix10.agoda.net/hotelImages/233/233840/233840_121227184850139.jpg?ca=0&ce=1&s=312x235&ar=16x9",
+                "https://cf.bstatic.com/xdata/images/hotel/max1024x768/575306044.jpg?k=35784defb18ac03f14d374e88ccf22beca297ecc26d84cf1ae0fbb84720235e4&o=&hp=1",
+                "https://media-cdn.tripadvisor.com/media/photo-s/15/66/c1/a0/dar-zemora.jpg"
+            ],
+            amenities: {
+                general: [
+                    "Air conditioning",
+                    "Concierge",
+                    "Gym",
+                    "Currency exchange facilities"
+                ],
+                entertainment: [
+                    "Children's pool",
+                    "Entertainment programme",
+                    "Outdoor pool",
+                    "TV lounge"
+                ],
+                foodAndDrinks: [
+                    "Bar",
+                    "Poolside snack bar",
+                    "Restaurant"
+                ]
+            }
+        },
+        cost: {
+            costPerCategory: {
+                holiday: 416.59
+            },
+            total: 416.59
+        },
+        holidayDetails: {
+            flights: {
+                outbound: {
+                    departureDate: "Mon 9th Sep 2024",
+                    reference: "EZY2265",
+                    from: "Manchester (MAN)",
+                    to: "Marrakech (RAK)",
+                    departureTime: "06:25",
+                    arrivalTime: "10:20"
+                },
+                return: {
+                    departureDate: "Thu 12th Sep 2024",
+                    reference: "TZY6765",
+                    from: "Marrakech (RAK)",
+                    to: "Manchester (MAN)",
+                    departureTime: "13:05",
+                    arrivalTime: "16:45"
+                }
+            },
+            seats: [
+                {
+                    name: "Ms Gerramie Gabo Castro",
+                    outbound: {
+                        seatNumber: "16F",
+                        seatType: " Rear Standard"
+                    },
+                    return: {
+                        seatNumber: "16A",
+                        seatType: " Rear Standard"
+                    }
+
+                }
+            ],
+            laggages: 1,
+            hotel: {
+                rooms: [
+                    {
+                        roomName: "SINGLE SUPERIOR",
+                        roomNumber: "34A",
+                        persons: 1
+                    }
+                ],
+                board: {
+                    boardName: "Half Board",
+                    includes: [
+                        "Breakfast",
+                        "Dinner"
+                    ]
+                }
+            },
+            passengers: [
+                {
+                    name: "Ms Gerramie Gabo Castro",
+                    email: "dreamlineholidays@hotmail.com",
+                    lead: true
+                }
+            ]
+        }
+    }
+}
